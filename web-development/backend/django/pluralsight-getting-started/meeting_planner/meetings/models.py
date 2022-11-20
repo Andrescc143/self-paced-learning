bbd2257d7725503560c2da9f505aacf040ef59b9
andrscc <andrescc143@gmail.com>
2022-11-20 18:18:25 -0500

    A new model Room was created, and a foreign key to stablish a relationship between it and the Meeting model was added to this last one. The database was re-started in order to make a clean deployment of the new models

--- web-development/backend/django/pluralsight-getting-started/meeting_planner/meetings/models.py
+++ web-development/backend/django/pluralsight-getting-started/meeting_planner/meetings/models.py
@@ -3,12 +3,21 @@ from datetime import time
 from django.db import models
 
 
+class Room(models.Model):
+    name = models.CharField(max_length=300)
+    floor = models.IntegerField()
+    room_number = models.IntegerField()
+    
+    def __str__(self):
+        return f"The room {self.name} with room number {self.room_number} at the {self.floor_number} floor. "
+    
+    
 class Meeting(models.Model):
     title = models.CharField(max_length=200)
     date = models.DateField()
     start_time = models.TimeField(default=time(9))
     duration = models.IntegerField(default=1)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
     
     def __str__(self):
         return f"{self.title} at {self.start_time} on {self.date}"
-    
