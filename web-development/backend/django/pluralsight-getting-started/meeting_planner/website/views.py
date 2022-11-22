c55528a834ff99b193aac917bbd98bed619f8fa8
andrscc <andrescc143@gmail.com>
2022-11-21 19:54:26 -0500

    The index.html template was changed to show the number of meeting objects created until execution.

--- web-development/backend/django/pluralsight-getting-started/meeting_planner/website/views.py
+++ web-development/backend/django/pluralsight-getting-started/meeting_planner/website/views.py
@@ -3,9 +3,12 @@ from django.http import HttpResponse
 
 from datetime import datetime
 
+from meetings.models import Meeting
+
 
 def index(request):
-    return render(request, "website/index.html", context={'message': 'This a simple message to fill up as a template variable.'})
+    num_meeting = Meeting.objects.count()
+    return render(request, "website/index.html", context={'num_meeting': num_meeting})
 
 
 def about(request):
