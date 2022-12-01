70a159e538aeaf1ce9619597ab8ccd4af318c39c
andrscc <andrescc143@gmail.com>
2022-12-01 15:43:59 -0500

    The view index was modified to set up a cookie to track the number of times the user has visit the page. The HTML template was modified as well to show the number of times mentioned before.

--- web-development/backend/django/pluralsight-views-app-and-url-mappings/globomantics/store/urls.py
+++ web-development/backend/django/pluralsight-views-app-and-url-mappings/globomantics/store/urls.py
@@ -6,5 +6,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     re_path(r'^\d+', views.detail, name='detail'),
-    re_path(r'^electronics', views.electronics, name='electronics')
+    re_path(r'^electronics', views.electronics, name='electronics'),
+    re_path(r'^classview/electronics', views.ElectronicsView.as_view(), name='electronics_classview')
 ]
--- web-development/backend/django/pluralsight-views-app-and-url-mappings/globomantics/store/views.py
+++ web-development/backend/django/pluralsight-views-app-and-url-mappings/globomantics/store/views.py
@@ -1,4 +1,5 @@
 from django.core.paginator import Paginator
+from django.views import View
 
 from django.shortcuts import render
 
@@ -6,8 +7,15 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("This is an e-commerce store. Here its home page.")
-
+    response = render(request, 'store/index.html')
+    if request.COOKIES.get('visits'):
+        value = int(request.COOKIES.get('visits'))
+        print('Getting cookie...')
+        response.set_cookie('visits', value + 1)
+    else:
+        response.set_cookie('visits', 1)
+    return response
+    
 
 def detail(request):
     return HttpResponse('This is the detail webpage.')
@@ -25,4 +33,16 @@ def electronics(request):
         return render(request, 'store/list.html', context={'items': items})
     else:
         HttpResponse(f'{request.method} Method not allowed.')
-    
+
+
+class ElectronicsView(View):
+    def get(self, request):
+        items = ('Windows PC', 'MAC', 'iPhone', 'Samsung A71', 'Macbook Pro 14"', 'PS4', 'XBOX')
+        paginator = Paginator(items, 2)
+        pages = request.GET.get('page', 1)
+        try:
+            items = paginator.page(pages)
+        except PageNotAnInteger:
+            items = paginator.page(1)
+        return render(request, 'store/list.html', context={'items': items})
+        
