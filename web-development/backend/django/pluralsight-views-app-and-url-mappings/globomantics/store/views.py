af339f58e3cf49cbb0fd31e22c10633ffcaf43a8
andrscc <andrescc143@gmail.com>
2022-12-01 13:04:44 -0500

    The view 'electronics' was modified to add a pagination behavior. The template 'list.html' was created to be rendered by this view, and the corresponding pagination logic was added.

--- web-development/backend/django/pluralsight-views-app-and-url-mappings/globomantics/store/views.py
+++ web-development/backend/django/pluralsight-views-app-and-url-mappings/globomantics/store/views.py
@@ -1,3 +1,5 @@
+from django.core.paginator import Paginator
+
 from django.shortcuts import render
 
 from django.http import HttpResponse
@@ -12,4 +14,15 @@ def detail(request):
 
 
 def electronics(request):
-    return HttpResponse('This is the electronics webpage.')
+    items = ['Windows PC', 'MAC', 'iPhone', 'Samsung A71', 'Macbook Pro 14"', 'PS4', 'XBOX']
+    if request.method == 'GET':
+        paginator = Paginator(items, 2)
+        pages = request.GET.get('page', 1)
+        try:
+            items = paginator.page(pages)
+        except PageNotAnInteger:
+            items = paginator.page(1)
+        return render(request, 'store/list.html', context={'items': items})
+    else:
+        HttpResponse(f'{request.method} Method not allowed.')
+    
