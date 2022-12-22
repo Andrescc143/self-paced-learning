9b41f07b0f214593ff2ad560b03716c631db14a8
andrscc <andrescc143@gmail.com>
2022-12-22 17:02:30 -0500

    The view Tickets was changed to show all the tickets created as a table. The view submit was changed as well to show

--- web-development/backend/django/pluralsight-templates/ticketing/ticketing/views.py
+++ web-development/backend/django/pluralsight-templates/ticketing/ticketing/views.py
@@ -1,14 +1,20 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from ticketing.models import Ticket
+
 
 def index(request):
     return render(request, 'index.html')
 
 
 def submit(request):
+    ticket = Ticket(submitter='Test_user', body="This is a ticket to use as an example.")
+    ticket.save()
+    print("Ticket saved correctly")
     return render(request, 'submit.html')
 
 
 def tickets(request):
-    return render(request, 'tickets.html')
+    tickets = Ticket.objects.all()
+    return render(request, 'tickets.html', context={'tickets': tickets})
