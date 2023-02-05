392a87b8184ec05541c533ac2d57f7616b9ea1fc
andrscc <andrescc143@gmail.com>
2023-02-05 14:39:58 -0500

    The files related to the Java course (introduction) made on Pluralsight were created

---
+++ java/pluralsight-getting-started-with-java-11/FirstProject/src/Main.java
@@ -0,0 +1,9 @@
+import Sum.Sum;
+
+public class Main {
+    public static void main(String[] args) {
+        List<>
+
+        //Given a list of integers, use the Stream API to filter out even numbers, square the remaining numbers, and return the sum of the squared numbers.
+    }
+}
