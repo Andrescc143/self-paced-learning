9c48d61b9f3586844a5f841a2df76098a2d82b1e
andrscc <andrescc143@gmail.com>
2023-02-05 14:40:39 -0500

    The course of JavaScript in Platzi was started and the first file to show primitive values was created

---
+++ javascript/platzi-javascript-introduction/valores.js
@@ -0,0 +1,13 @@
+// PRIMITIVES TYPE OF VALUES
+
+40 //números
+"Hola soy un texto" //cadenas de texto
+
+true // booleano
+false // booleano
+
+null //nulo
+undefined //para guardar espacio en memoria sin definir su tipo de dato. No recomendado
+
+[1,2,3] // Array (Objeto)
+{nombre: "Diego"} // Objeto
