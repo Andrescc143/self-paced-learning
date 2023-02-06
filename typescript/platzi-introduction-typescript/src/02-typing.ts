79666968e6b3aec373a7c8cf39a74d295589c70c
andrscc <andrescc143@gmail.com>
2023-02-05 23:16:47 -0500

    Typescript-introduction: A short example to see the static typing engine and how it assign a type to the variables automatically, was implemented in the 02-typing.ts file

---
+++ typescript/platzi-introduction-typescript/src/02-typing.ts
@@ -0,0 +1,14 @@
+/*The below syntax with the arrow function is used to isolate the environment of execution of the program and create an independent scope.*/
+(()=> {
+//The engine is capable to identify the variable type
+let myProductName = "Product1";
+let productPrice = 43;
+
+//Even when the type is automatically selected by the engine, we can not use any other type of variable for each variable in the rest of the code, otherwise, it will throws an error.
+//myProductName = 456;
+
+//The same behavior can be seen if the placeholder is not variable but const
+const productName = "Car";
+//productName = "Car2";
+})()
+
