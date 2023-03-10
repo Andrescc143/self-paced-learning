65fa3d2fe8f902b1c02417464e9a43a2cd2a4406
andrscc <andrescc143@gmail.com>
2023-03-10 16:43:56 -0500

    Pluralsight: Last update from the course of Java, getting started

---
+++ java/pluralsight-getting-started-with-java-11/LogicalOperators/src/main/java/main.java
@@ -0,0 +1,10 @@
+public class main {
+    public static void main(String[] args) {
+        int age = 19;
+        String name = "Camilo";
+
+        if (age > 18 && name != "Camil"){
+            System.out.println("User registered correctly");
+        }
+    }
+}
