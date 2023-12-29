2e906a22794496a582914d54a0336b12f5645d35
andrscc <andrescc143@gmail.com>
2023-12-28 21:36:44 -0500

    Updating the last changes from  other Java courses

---
+++ java/amigoscode-introduction/exercises/HelloWorld/HelloWorld/src/Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+    public static void main(String[] args) {
+        double pi = Math.PI;
+
+        System.out.println("The PI number: " + pi);
+        System.out.println("The PI number rounded up: " + (int) pi);
+    }
+}
