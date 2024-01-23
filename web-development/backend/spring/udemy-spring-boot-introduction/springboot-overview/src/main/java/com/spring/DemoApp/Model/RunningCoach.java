a26a8f50fc4f11fd06bd954eafab4acfe055c121
andrscc <andrescc143@gmail.com>
2024-01-23 08:22:47 -0500

    udemy-spring-boot-introduction/Lazy annotation was covered with these changes as well. Some modification were applied over the constructors of each class to probe in a better way the principle behind Lazy initialization.

--- web-development/backend/spring/udemy-spring-boot-introduction/springboot-overview/src/main/java/com/spring/DemoApp/Model/RunningCoach.java
+++ web-development/backend/spring/udemy-spring-boot-introduction/springboot-overview/src/main/java/com/spring/DemoApp/Model/RunningCoach.java
@@ -1,10 +1,14 @@
 package com.spring.DemoApp.Model;
 
+import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
 public class RunningCoach implements Coach{
+    public RunningCoach (){
+        System.out.println("In constructor: " + getClass().getSimpleName());
+    }
     @Override
     public String getDailyWorkout() {
         System.out.println("In constructor: " + getClass().getSimpleName());
--- web-development/backend/spring/udemy-spring-boot-introduction/springboot-overview/src/main/java/com/spring/DemoApp/Model/SoccerCoach.java
+++ web-development/backend/spring/udemy-spring-boot-introduction/springboot-overview/src/main/java/com/spring/DemoApp/Model/SoccerCoach.java
@@ -6,9 +6,11 @@ import org.springframework.stereotype.Component;
 @Component
 @Lazy
 public class SoccerCoach implements Coach{
+    public SoccerCoach (){
+        System.out.println("In constructor: " + getClass().getSimpleName());
+    }
     @Override
     public String getDailyWorkout (){
-        System.out.println("In constructor: " + getClass().getSimpleName());
         return "Here is your Soccer workout for today !";
     }
 }
